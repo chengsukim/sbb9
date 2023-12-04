7c3247ee784fd4380fedd62d92964ac715bc2b4e
깃chengsukim <kcs981123@naver.com>
2023-12-04 09:30:56 +0900

    sbb9

---
+++ src/main/java/com/example/sbb9/Sbb9Application.java
@@ -0,0 +1,13 @@
+package com.example.sbb9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbb9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbb9/Sbb9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbb9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
